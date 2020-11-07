96ea6ad5e657a17f382a39f3c5dd81a250b61f71
Clayton Pereira <cpereiramt@gmail.com>
2020-11-07 19:16:58 -0400

    implementation of navigation

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/es/integration/react';
 import {store, persistor} from './android/app/src/store/index';
-import HomeScreen from './android/app/src/screens/PlanetList';
+import PlanetList from './android/app/src/screens/PlanetList';
+import PlanetDetails from './android/app/src/screens/PlanetDetails';
 
 // console.disableYellowBox = true;
 
 const App = () => {
+  const Stack = createStackNavigator();
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <HomeScreen />
-      </PersistGate>
-    </Provider>
+    <NavigationContainer>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={PlanetList} />
+            <Stack.Screen name="Details" component={PlanetDetails} />
+          </Stack.Navigator>
+        </PersistGate>
+      </Provider>
+    </NavigationContainer>
   );
 };
 
